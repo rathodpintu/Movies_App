f22e8b5793da2d23e7a944319a04e3a18799cd05
Pintu Rathod <60134479+rathodpintu@users.noreply.github.com>
2022-08-12 16:49:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+//import AssignmentApp from './AssignmntRedux/AppAssignment';
+import {name as appName} from './app.json';
+import App from './pintu/Assignment/App';
+
+AppRegistry.registerComponent(appName, () => App);
